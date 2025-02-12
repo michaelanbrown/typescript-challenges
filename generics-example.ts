bd41ef7a33708da7fcd0fab5b9ee76f816bebd36
michaelanbrown <m_brownbrown@yahoo.com>
2025-02-12 09:56:04 -0500

    Added an example for generics and a possible challenge

---
+++ generics-example.ts
@@ -0,0 +1,9 @@
+//Explore generics by creating a generic function that can work with different types.
+// The challenge is to write a function that swaps the values of two variables regardless of their types.
+
+function swap<T>(a: T, b: T): [T, T] {
+    return [b, a];
+  }
+  
+  console.log(swap<number>(1, 2)); // Output: [2, 1]
+  console.log(swap<string>('hello', 'world')); // Output: ["world", "hello"]
