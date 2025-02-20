c0f3dc4ba3f8c13d81d6c2c4745a92466838716d
michaelanbrown <m_brownbrown@yahoo.com>
2025-02-20 15:46:08 -0500

    Fixed spacing typos

--- enums-example.ts
+++ enums-example.ts
@@ -2,20 +2,20 @@
 // The challenge is to create an enum for days of the week and write a function that returns the full name of a given day.
 
 enum DaysOfWeek {
-    Monday = 'Monday',
-    Tuesday = 'Tuesday',
-    Wednesday = 'Wednesday',
-    Thursday = 'Thursday',
-    Friday = 'Friday',
-    Saturday = 'Saturday',
-    Sunday = 'Sunday',
-  }
+  Monday = 'Monday',
+  Tuesday = 'Tuesday',
+  Wednesday = 'Wednesday',
+  Thursday = 'Thursday',
+  Friday = 'Friday',
+  Saturday = 'Saturday',
+  Sunday = 'Sunday',
+}
   
-  function getDayFullName(day: DaysOfWeek): string {
-    return day;
-  }
+function getDayFullName(day: DaysOfWeek): string {
+  return day;
+}
   
-  console.log(getDayFullName(DaysOfWeek.Sunday)); // Output: Sunday
+console.log(getDayFullName(DaysOfWeek.Sunday)); // Output: Sunday
 
 // Make enums for months in a year
 
